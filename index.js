96fd4882995d09fed77a93af8ffcf70d8addd45a
Pete Schaffner <pschaffner@google.com>
2015-05-13 15:03:23 +0200

    Create base functionality with light/dark themes

--- index.js
+++ index.js
@@ -1,53 +1,99 @@
+
+/**
+ * Module dependencies.
+ */
+
 var Label = require('framer-uilabel')
-var dirname = __dirname == '/' ? '.' : __dirname
+
+/**
+ * iOS status bar for your prototyping pleasure.
+ *
+ * @class
+ * @param {Object} options
+ * @param {string} options.style=dark - `dark` means "dark content"
+ */
 
 module.exports = class StatusBar extends Layer {
   constructor(options={}) {
-    options.theme = options.theme || 'black'
+    options.style = options.style || 'dark'
     options.contentScale = options.contentScale || Framer.Device.contentScale
+
+    var padding = 12 / options.contentScale
+    var fontSize = `${24 / options.contentScale}px`
 
     super(_.extend(options, {
       name: 'statusBar',
       width: Framer.Screen.width / options.contentScale,
-      height: 40 / options.contentScale
-      // backgroundColor: 'transparent'
+      height: 40 / options.contentScale,
+      backgroundColor: 'transparent'
     }))
 
-    // Networks
-    new Layer({
-      name: 'network',
+    var networkSignal = new Layer({
+      name: 'networkSignal',
       superLayer: this,
       height: 12 / options.contentScale,
       width: 68 / options.contentScale,
-      x: 12 / options.contentScale,
-      image: `${dirname}/images/statusBarSignal.png`
+      x: padding,
+      midY: this.midY,
+      image: './images/statusBarSignal.png'
     })
 
-    new Label({
+    var networkName = new Label({
+      name: 'networkName',
+      superLayer: this,
+      x: networkSignal.maxX + 8 / options.contentScale,
+      midY: this.midY,
+      text: 'Framer',
+      fontSize: fontSize,
+      color: 'black'
+    })
+
+    var wifi = new Layer({
+      name: 'wifi',
+      superLayer: this,
+      width: 28 / options.contentScale,
+      height: 20 / options.contentScale,
+      x: networkName.maxX + 8 / options.contentScale,
+      midY: this.midY,
+      image: './images/statusBarWifi.png'
+    })
+
+    var time = new Label({
       name: 'time',
       superLayer: this,
       midX: this.midX,
       midY: this.midY,
       text: '9:41 AM',
-      fontSize: '12px',
-      backgroundColor: 'red'
+      fontSize: fontSize,
+      color: 'black'
     })
-    //
-    // new Layer({
-    //   name: 'battery',
-    //   superLayer: this,
-    //   height: 20 / options.scale,
-    //   width: 50 / options.scale,
-    //   maxX: this.maxX - (12 / options.scale),
-    //   image: __dirname + '/images/statusBarBattery.png'
-    // })
 
-    this.theme = options.theme
+    var battery = new Layer({
+      name: 'battery',
+      superLayer: this,
+      width: 50 / options.contentScale,
+      height: 20 / options.contentScale,
+      maxX: this.width - padding,
+      midY: this.midY,
+      image: './images/statusBarBattery.png'
+    })
+
+    var batteryPercentage = new Label({
+      name: 'batteryPercentage',
+      superLayer: this,
+      text: '100%',
+      maxX: battery.x - 6 / options.contentScale,
+      midY: this.midY,
+      fontSize: fontSize,
+      color: 'black'
+    })
+
+    this.style = options.style
   }
 
-  // get theme() { return this._theme }
-  // set theme(value) {
-  //   this._theme = value
-  //   this.invert = (value == 'black') ? 0 : 100
-  // }
+  get style() { return this._style }
+  set style(value) {
+    this._style = value
+    this.invert = (value == 'dark') ? 0 : 100
+  }
 }
